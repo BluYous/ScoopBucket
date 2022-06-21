71b9036d86e926f779b993616789f40b5d27e329
colenc <colenc@synnex.com>
2022-06-21 12:27:19 +0800

    update dbeaver-enterprise-free

--- scripts/dbeaver-enterprise-portable.ps1
+++ scripts/dbeaver-enterprise-portable.ps1
@@ -4,6 +4,7 @@ $bucketsdir = $bucketsdir -replace '\\', '/'
 
 $CONT = Get-Content "$dir\dbeaver.ini"
 $CONT += "-javaagent:$bucketsdir/BluYousScoopBucket/assets/dbeaver-agent/dbeaver-agent.jar"
+$CONT += "-Dlm.debug.mode=true"
 Set-Content -LiteralPath "$dir\dbeaver.ini" -Value $CONT -Encoding 'Ascii' -Force
 
 Remove-Item -Force -Recurse "$dir\jre"
